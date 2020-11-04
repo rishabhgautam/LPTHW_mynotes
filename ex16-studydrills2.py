eb462926ab924ea1096e0e81125e94f338b2ddd2
Rishabh Gautam <rishabh.gautam@irco.com>
2020-11-04 15:03:27 +0530

    adding chapters

---
+++ ex16-studydrills2.py
@@ -0,0 +1,11 @@
+# A script similar to ex16 that uses read and argv to read the file
+
+from sys import argv
+
+script, filename = argv
+
+print("The contents of the file %s:" % filename)
+target = open(filename)
+contents = target.read()
+print(contents)
+target.close()
